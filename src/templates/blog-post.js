879703c09ec36790e7f5c895d4306472615b4a2b
Karl-Sander Erss <karl.erss@gmail.com>
2021-03-22 16:36:40 +0200

    Add MC.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -33,6 +33,27 @@ const BlogPostTemplate = ({ data, location }) => {
         <hr />
         <footer>
           <Bio />
+          <hr />
+          <div dangerouslySetInnerHTML={{__html: `<!-- Begin Mailchimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+\t#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+\t/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+\t   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="https://karlerss.us1.list-manage.com/subscribe/post?u=11780be201dd8dab8321a6aed&amp;id=904d661daf" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+\t<label for="mce-EMAIL">Get notified about new posts</label>
+\t<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_11780be201dd8dab8321a6aed_904d661daf" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->`}}></div>
         </footer>
       </article>
       <nav className="blog-post-nav">

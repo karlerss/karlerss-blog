73327705270f2e9429d2ad7e3e9bb576d77de73d
Karl-Sander Erss <karl.erss@gmail.com>
2025-02-09 18:54:22 +0200

    Update bio.js

--- src/components/bio.js
+++ src/components/bio.js
@@ -48,7 +48,9 @@ const Bio = () => {
                   Written by <strong>{author.name}</strong> {author?.summary || null}
                   {` `} Co-founder at <a href={'https://www.teamdash.com/'} target={'_blank'}>Teamdash</a>.
                   Drop me a line on <a href={'https://www.linkedin.com/in/karl-sander-erss/'} target={'_blank'}>linkedin</a>. 
-                  Check out my <a href={'https://utm.zone/'} target={'_blank'}>Bulk UTM link builder</a>.
+                  Check out my <a href={'https://utm.zone/'} target={'_blank'}>Bulk UTM link builder</a>,
+                  <a href={'https://raamatud.karlerss.com/'} target={'_blank'}>reading journal</a>,
+                  <a href={'https://dokumendiregistrid.karlerss.com/'} target={'_blank'}>full text search from government documents</a>.
               </p>
           </div>
       )}
